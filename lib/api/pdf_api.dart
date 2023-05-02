f189c03a0f5f96cacbb295255a629944009b80e4
Fareed-Ahmad7 <fareedahmad1097@gmail.com>
2023-05-02 15:26:02 +0530

    solve pdf open  android

--- lib/api/pdf_api.dart
+++ lib/api/pdf_api.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:open_file/open_file.dart';
+import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/widgets.dart';
 
@@ -22,6 +22,6 @@ class PdfApi {
   static Future openFile(File file) async {
     final url = file.path;
 
-    await OpenFile.open(url);
+    await OpenFilex.open(url);
   }
 }
